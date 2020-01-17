cef45cd2293db1e70f667c2e7ac531b6d8baa42f
jpeter78 <57476908+jpeter78@users.noreply.github.com>
2020-01-17 15:52:26 +0800

    Create name.py

---
+++ chapter02/name.py
@@ -0,0 +1,13 @@
+name = 'ada lovelace'
+print(name.title())
+name = name.title()
+print(name)
+print(name.upper())
+print(name.lower())
+first_name = 'ada'
+last_name = 'lovelace'
+full_name = first_name + ' ' + last_name
+print(full_name)
+print('Hello,' + full_name.title() + '!')
+message = 'Hello,' + full_name.title() + '!'
+print(message)
